f07875a5f4663ec2816aa6ff5dd0ae8126e00308
John William Davis <jwdavisdeveloper@gmail.com>
2022-02-16 15:51:14 -0800

    Finish Nextjs official tutorial

---
+++ pages/thoughts/[id].js
@@ -0,0 +1,37 @@
+import Layout from "../../src/components/layout.js";
+import { getAllPostIds, getPostData } from "../../lib/posts.js";
+import Head from "next/head";
+import Date from "../../src/components/date.js";
+import utilStyles from "../../styles/utils.module.css";
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export default function Thought({ postData }) {
+  return (
+    <Layout>
+      <Head>{postData.title}</Head>
+      <article>
+        <h1 className={utilStyles.headingXL}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
